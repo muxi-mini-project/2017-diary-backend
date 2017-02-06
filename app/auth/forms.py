30d0959af43dc616dd0a7e262d3e8ecac83f0f4f
yuyilei <yyl1352091742@gmail.com>
2017-02-06 18:21:25 +0800

    login logout register

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,12 +1,36 @@
 # coding: utf-8
 
 from flask_wtf import Form
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
-
+from wtforms import StringField, PasswordField, SubmitField , BooleanField 
+from wtforms.validators import DataRequired ,Email , Length 
+from ..models import User 
+from wtforms import ValidationError 
 
 class LoginForm(Form):
-    username = StringField('username', validators=[DataRequired()])
     password = PasswordField('password', validators=[DataRequired()])
-    submit = SubmitField('login!')
+    email = StringField('email',validators=[DataRequired(), Email(), Length(1,64) ]
+    remember_me = BooleanField('记住我')
+    submit = SubmitField('登录')
+    
+
+#用户注册表单
+class Register(Form):
+    email = StringField('email',validators=[DataRequired() , Length(1,64),Email()])
+    username =
+    StringField('username',validators=[DataRequired(),Length(1,64),Regexp('^[A-Za-z][A-Za-z0-9_.]*$',0,'用户名必须只包括数字,字母,下划线或点号.')])
+    password = PasswordField('password',validators=[DataRequired(),EqualTo('password2',message='两次密码输入不一致')])
+
+    passwod2 = PasswordField('确认密码' , validators=[DataRequired()])
+    submit = SubmitField('注册')
+
+    def validate_email(self,field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('邮箱已被注册!')
+
+    def validate_username(self,field) :
+        if User.query.filter_by(username=field.data).first() :
+            raise ValidationError('用户已存在!')
+    
+
+
 
--- app/auth/views.py
+++ app/auth/views.py
@@ -6,21 +6,34 @@ from flask_login import login_user, logout_user, current_user, login_required
 from app.models import User
 from .forms import LoginForm
 
-
+#登录路由
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username = form.username.data).first()
+        user = User.query.filter_by(email=form.email.data).first()
         if user is not None and user.verify_password(form.password.data):
-            login_user(user)
-            return redirect('/admin')
+            login_user(user,form.remember.data)
+            return redirect('main.index')
+        flash('Invaild email or password.')
     return render_template('auth/login.html', form=form)
 
-
+#用户登出路由
 @login_required
 @auth.route('/logout')
 def logout():
     logout_user()
+    flash('You have logged out !')
     return redirect(url_for('auth.login'))
 
+#用户注册路由 
+@auth.route('/register',methods=['GET','POST'])
+def register() :
+    form = Register() 
+    if form.validate_on_submit() :
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.passwors.data)
+        db.session.add(user)
+        db.session.commit()
+        
