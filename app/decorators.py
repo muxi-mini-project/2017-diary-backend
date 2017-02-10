c5441a0cc31cd6c24b7680533766c9e2007b593b
yuyilei <yyl1352091742@gmail.com>
2017-02-10 23:27:10 +0800

    admin_required

---
+++ app/decorators.py
@@ -0,0 +1,23 @@
+from functools import wraps 
+from flask import abort , g  , request , jsonify  
+from app.models import User
+from flask_login import current_user 
+import base64
+
+def admin_required(f) :
+    @wraps(f)
+    def decorated(*args.**kwargs) :
+        token_header = request.headers.get('Authorization',None)
+        if token_header  :
+            token_hash = token_header[6:]
+            decode_token = base64.b64decode(token_hash)
+            token = decode_token[:-1]
+            g.current_user = User.verify_auth_token(token)
+            if not g.current_user.is_administrator() :
+                return jsonify({'message' : '403 Forbidden'}) , 403
+            return f(*args, **kwargs)
+        else :
+            return jsonify({'message':'401 unAuthorization'}) ,401
+    return decorated 
+
+            
