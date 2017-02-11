5bbfbeb11d4448f4e0a5f196bb30061080b63daa
yuyilei <yyl1352091742@gmail.com>
2017-02-11 11:23:19 +0800

    confusing ,too

---
+++ app/api_1_0/like.py
@@ -0,0 +1,17 @@
+from flask import jsonify, Response , g ,request
+import json 
+from ..models import Like 
+from . import api
+from .. import db 
+
+@api.route('/like/post/' ,methods=['POST','GET'])
+def like_post() : 
+    if request.method == 'POST' :
+        like = Like()
+        like.post_id = int(request.get_json().get('post_id'))
+        db.session.add(like)
+        db.session.commit()
+        return jsonify({ 'status' : 200 
+                          })
+        
+
